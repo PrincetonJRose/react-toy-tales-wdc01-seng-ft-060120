e6cc2c5120f114ac37e80d87ed799d1a0f8fd790
Princeton Rose <princetonjrose@gmail.com>
2020-11-09 18:39:39 -0500

    finished the practice lab yay! ^_^

--- src/App.js
+++ src/App.js
@@ -8,11 +8,18 @@ import ToyContainer from './components/ToyContainer'
 const baseUrl = `http://localhost:3000/`
 const toysUrl = baseUrl + `toys/`
 
+const requestHeader = {
+  'content-type': 'application/json',
+  Accepts: 'application/json'
+}
+
+
 
 const App =( )=> {
 
   const [ display, setDisplay ] = useState( false )
   const [ toys, setToys ] = useState( [] )
+  const [ newToy, setNewToy ] = useState( { likes: 0 } )
 
   useEffect( () => {
     fetch( toysUrl )
@@ -21,19 +28,47 @@ const App =( )=> {
   }, [] )
 
   const increaseLikes =( likedToy )=> {
-    setToys( toys.map( toy => {
-        if ( toy.id === likedToy.id )
-          toy.likes += 1
-        return toy
-      })
+    likedToy.likes += 1
+
+    const patchRequest = {
+      method: 'PATCH',
+      headers: requestHeader,
+      body: JSON.stringify( likedToy )
+    }
+
+    fetch( toysUrl + likedToy.id, patchRequest )
+    .then( res => res.json() )
+    .then( updatedToy => 
+      setToys( toys.map( toy => {
+          if ( toy.id === updatedToy.id )
+            return updatedToy
+          return toy
+        })
+      )
     )
+  }
+
+  const createToy =( )=> {
+    const postRequest = {
+      method: 'POST',
+      headers: requestHeader,
+      body: JSON.stringify( newToy ) 
+    }
+
+    fetch( toysUrl, postRequest )
+    .then( res => res.json() )
+    .then( createdToy => setToys( [...toys, createdToy ] ) )
   }
 
   return (
     <>
       <Header/>
       { display ?
-        <ToyForm/>
+        <ToyForm
+          setNewToy = { setNewToy }
+          newToy = { newToy }
+          createToy = { createToy }
+        />
           :
         null
       }
--- src/components/ToyForm.jsx
+++ src/components/ToyForm.jsx
@@ -2,13 +2,29 @@ import React, { useEffect, useState } from 'react';
 
 const ToyForm =( props )=> {
 
+  const setNewToy = props.setNewToy
+  const newToy = props.newToy
+
+  const changeToy =( e )=> {
+    setNewToy( {...newToy, [e.target.name]: e.target.value } )
+  }
+
   return (
     <div className="container">
-      <form className="add-toy-form">
+      <form className="add-toy-form"
+        onSubmit = { (e) => {
+          e.preventDefault()
+          props.createToy()
+        }}
+      >
         <h3>Create a toy!</h3>
-        <input type="text" name="name" placeholder="Enter a toy's name..." className="input-text"/>
+        <input type="text" name="name" placeholder="Enter a toy's name..." className="input-text"
+          onChange = { changeToy } required
+        />
         <br/>
-        <input type="text" name="image" placeholder="Enter a toy's image URL..." className="input-text"/>
+        <input type="text" name="image" placeholder="Enter a toy's image URL..." className="input-text"
+          onChange = { changeToy } required
+        />
         <br/>
         <input type="submit" name="submit" value="Create New Toy" className="submit"/>
       </form>
